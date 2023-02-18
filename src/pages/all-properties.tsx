68f15850b4c0c586b1ea5491c7ffae7479437fb8
halisa <halisasaipulaa@gmail.com>
2023-02-18 13:06:19 -0800

    created property card and rendered all properties

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,11 +1,20 @@
 import { Add } from '@mui/icons-material';
-import { useList } from '@pankod/refine-core';
+import { useTable } from '@pankod/refine-core';
 import  { Box, Stack, Typography } from '@pankod/refine-mui';
 import { useNavigate } from '@pankod/refine-react-router-v6';
 import { PropertyCard, CustomButton } from 'components';
 
 const AllProperties = () => {
     const navigate = useNavigate();
+
+    const {
+        tableQueryResult: { data, isLoading,isError}
+    } = useTable()
+    console.log(data);
+    const allProperties = data?.data?? [];
+    if(isLoading) return <Typography>Loading...</Typography>
+    if(isError) return <Typography>Error..</Typography>
+
     return (
         <Box>
             <Stack direction="row" justifyContent="space-between"alignItems="center">
@@ -14,6 +23,19 @@ const AllProperties = () => {
                     title="Add Property"  handleClick={()=> navigate('/properties/create')}
                     backgroundColor="#475be8" color="#fcfcfc" icon={<Add />}/>
             </Stack>
+
+            <Box mt="20px" sx={{ display:'flex', flexwrap:'wrap', gap:3}}>
+                {allProperties.map((property)=>(
+                    <PropertyCard
+                        key={property._id}
+                        id={property._id}
+                        title={property.title}
+                        price={property.price}
+                        location={property.location}
+                        photo={property.photo}
+                    />
+                ))}
+            </Box>
         </Box>
     )
 }
