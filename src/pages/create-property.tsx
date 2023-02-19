f95beb079b571a57722f37d6911ed18157cbe149
halisa <halisasaipulaa@gmail.com>
2023-02-19 09:13:51 -0800

    rendered the property detail on the page

--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -15,7 +15,7 @@ const CreateProperty = () => {
             const fileReader = new FileReader();
             fileReader.onload = () => resolve(fileReader.result as string);
             fileReader.readAsDataURL(readFile);
-          });
+        });
 
         reader(file).then((result: string) => setPropertyImage({ name: file?.name, url: result }));
     }
