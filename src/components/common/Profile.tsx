abcdb96cbf22d20fa2498517436c9667850710de
halisa <halisasaipulaa@gmail.com>
2023-02-19 12:09:20 -0800

    successfully rendered all properties on user's profile page

--- src/components/common/Profile.tsx
+++ src/components/common/Profile.tsx
@@ -162,6 +162,33 @@ const Profile = ({ type, name, avatar, email, properties }: ProfileProps)  => {
             </Box>
         </Box>
         
+        {properties.length > 0 && (
+            <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#FCFCFC">
+                <Typography fontSize={18} fontWeight={600} color="#11142D">
+                    {type} Properties
+                </Typography>
+
+                <Box
+                    mt={2.5}
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        gap: 2.5,
+                    }}
+                >
+                    {properties?.map((property: PropertyProps) => (
+                        <PropertyCard
+                            key={property._id}
+                            id={property._id}
+                            title={property.title}
+                            location={property.location}
+                            price={property.price}
+                            photo={property.photo}
+                        />
+                    ))}
+                </Box>
+            </Box>
+        )}
 
     </Box>
     )
